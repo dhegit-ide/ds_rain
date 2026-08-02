9e3e09f639df56a36b65ede9cd1283e7a9c27296
Dhea Gita <dheagita499@gmail.com>
2026-08-02 22:04:52 +0800

    add plot_series

--- components/tab2_forecast/row2_preview_cv.py
+++ components/tab2_forecast/row2_preview_cv.py
@@ -12,12 +12,11 @@ def render(cv_df):
 
     with st.container(border=True):
         st.subheader(":material/show_chart: Visualisasi Forecast")
+        st.caption(":material/info: Gunakan kontrol di sidebar untuk memilih Unique ID")
         pred_cols = [c for c in cv_df.columns if c not in ['unique_id','ds','cutoff','y']]
-
         if len(pred_cols) == 0:
             st.warning(":material/warning: Tidak ada kolom hasil prediksi model ditemukan.")
             return
-
         fig = plot_series(
                     df=cv_df[['unique_id','ds','y']],
                     forecasts_df=cv_df.drop(columns=['cutoff', 'y']),
@@ -26,5 +25,19 @@ def render(cv_df):
         )
         fig.update_layout(showlegend=True)
         st.plotly_chart(fig, use_container_width=True)
+        
+        st.subheader(":material/show_chart: Visualisasi Forecast (100 data terakhir)")
+        st.caption(":material/info: Gunakan kontrol di sidebar untuk memilih Unique ID")
+        last_100_ds = cv_df['ds'].drop_duplicates().nlargest(100)
+        df_filtered = cv_df[(cv_df['unique_id'] == selected_unique_id) & (cv_df['ds'].isin(last_100_ds))]
+
+        fig2 = plot_series(
+            df=df_filtered[['unique_id','ds','y']],
+            forecasts_df=df_filtered[['unique_id','ds'] + pred_cols],
+            ids=[selected_unique_id],
+            engine='plotly'
+        )
+        fig2.update_layout(showlegend=True)
+        st.plotly_chart(fig2, use_container_width=True)
     
     st.divider()
